8eeb3d2f6d46e5d033bdb7b891ab4dde32db04a3
mecwilly <47599733+mecwilly@users.noreply.github.com>
2019-02-14 23:23:28 +0100

    Update damerino_scratch_blocks_v1.1.js

--- damerino_scratch_blocks_v1.1.js
+++ damerino_scratch_blocks_v1.1.js
@@ -32,6 +32,49 @@
         });
     };
 
+       ext.camera = function(status, callback) {
+        $.ajax({
+              url: damerino_server_address + 'camera%23'+status,
+              type: 'GET',
+              success: function(data) {
+                  callback(data);
+              }
+        });
+    };
+
+    ext.tracking = function(status, reverse_status, callback) {
+        $.ajax({
+              url: damerino_server_address + 'tracking%23'+status+'%23'+reverse_status,
+              type: 'GET',
+              success: function(data) {
+                  callback(data);
+              }
+        });
+    };
+
+    ext.listen = function(language, callback) {
+        $.ajax({
+              url: damerino_server_address + 'listen%23'+language,
+              type: 'GET',
+              success: function(data) {
+                  callback(data);
+              }
+        });
+    };
+
+    ext.say = function(text, language, callback) {
+        $.ajax({
+              url: damerino_server_address + 'say%23'+text+'%23'+language,
+              type: 'GET',
+              success: function(data) {
+                  callback(data);
+              }
+        });
+    };
+    
+    
+    
+    /*
     ext.cameraDue = function(state, callback) {
         $.ajax({
               url: 'http://127.0.0.1:8888/?camera%23'+state,
@@ -61,21 +104,25 @@
               }
         });
     };
-
+*/
+    
+    
     var descriptor = {
         blocks: [
-            ['R', 'DAMERINO say %m.di città %s', 'say', 'ciao', 'Rome'],
-            ['R', 'DAMERINO camera %m.activate', 'camera', 'on'],
-            ['w', 'DAMERINO camera2 %m.activate', 'cameraDue', 'on']
+['R', 'DAMERINO say %t language %m.lan_tts', 'say', 'I'm Damerino', 'en'],
+            ['R', 'DAMERINO camera %m.on_off', 'camera', 'on'],
+            ['R', 'DAMERINO tracking %m.on_off reverse %m.on_off', 'tracking', 'on', 'off'],
+            ['R', 'DAMERINO listen language %m.lan_asr', 'listen', 'en']
         ],
         menus: {
-        di: ['hi','hi friend'],
-        activate: ['on','off']
+        on_off: ['on','off'],
+        lan_asr: ['en','it','de','fr','es'],
+        lan_tts: ['en','it','de','fr','es']
     },
     url: 'http://mecwilly.it/damerino',
     displayName: 'Damerino Robot 3.0'
     };
 
     // Register the extension
-    ScratchExtensions.register('Damerino extension', descriptor, ext);
+    ScratchExtensions.register('Damerino robot extension', descriptor, ext);
 })({});
